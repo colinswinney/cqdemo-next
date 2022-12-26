421daab27f34162a6029f0ecceeb0cb40c89c742
Colin Swinney <colinjswinney@gmail.com>
2022-12-25 22:20:18 -0600

    add highlight.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import hljs from "highlight.js";
+
+// styles
+import "../styles/reset.css";
+import "highlight.js/styles/a11y-dark.css";
+import "../styles/globals.css";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		hljs.highlightAll();
+	}, []);
+
+	return <Component {...pageProps} />;
 }
