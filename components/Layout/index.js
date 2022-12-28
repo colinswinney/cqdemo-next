ed076753bb69ab3f0dc62f630134f9c7c13e1a5d
Colin Swinney <colinjswinney@gmail.com>
2022-12-28 07:33:30 -0600

    layout component

---
+++ components/Layout/index.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children, metaDescription, metaTitle, title }) {
+	return (
+		<>
+			<Head>
+				<title>{`Container Queries | ${metaTitle}`}</title>
+				<meta name="description" content={metaDescription} />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<header className="banner">
+				<Link href="/" className="logo">
+					Container Queries
+				</Link>
+			</header>
+			<main className="main">
+				<div className="main-container">
+					<div className="page-header">
+						<h1 dangerouslySetInnerHTML={{ __html: title }} />
+					</div>
+					{children}
+				</div>
+			</main>
+			<footer className="content-info">
+				<nav>
+					<ul>
+						<li>
+							<Link href="/lesson-one">
+								Lesson One - What Do They Look Like?
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</footer>
+		</>
+	);
+}
