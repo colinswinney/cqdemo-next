f9bdbe414640d976ba2e7969ab74293adf8cdbb1
Colin Swinney <colinjswinney@gmail.com>
2022-12-29 13:11:16 -0600

    refactor quote block

--- components/QuoteBlock/index.js
+++ components/QuoteBlock/index.js
@@ -1,15 +1,28 @@
-export default function QuoteBlock({className, ...restProps}) {
+export default function QuoteBlock({className, paragraph, cite, ...restProps}) {
 	return (
 		<blockquote
 			className={`wp-block-quote${className ? " " + className : ""}`}
 			{...restProps}
 		>
-			<p>
-				CSS containment provides a way to isolate parts of a page and declare to
-				the browser these parts are independent from the rest of the page in
-				terms of styles and layout.
-			</p>
-			<cite>MDN Web Docs</cite>
+			<p
+				dangerouslySetInnerHTML={{
+					__html: paragraph
+						? paragraph
+						: "CSS containment provides a way to isolate parts of a page and declare to the browser these parts are independent from the rest of the page in terms of styles and layout.",
+				}}
+			/>
+			{ paragraph && !cite
+				? ""
+				: <cite
+					dangerouslySetInnerHTML={{
+						__html: !paragraph
+							? "MDN Web Docs"
+							: cite
+							? cite
+							: "",
+					}}
+				/>
+			}
 		</blockquote>
 	);
 }
